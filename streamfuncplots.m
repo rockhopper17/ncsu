d07b15529c4338d0853b0d48a18b4478af47a1db
Andrew Navratil <drew.navratil@gmail.com>
2018-03-30 09:08:21 -0400

    all plots with contour labels

--- streamfuncplots.m
+++ streamfuncplots.m
@@ -6,147 +6,143 @@
 % clear all vars and plots
 close all; clear all; clc;
 
-%=====================================================================
-% Rankine Half Oval
-%=====================================================================
-
 % variables
 vInf = 50;					% freestream velocity (m/s)
-lambda = 50;				% source / sink strenth
-b = 2;						% distance between sources
-x0 = 1;						% x center coord
-y0 = .5;					% y center coord
+lambda = 50;				% source / sink strength
+b = .25;					% distance from center to source / sink
+R = .25;					% cylinder radius where R^2 = kappa / (2pi vInf);
+circ = 4*pi*vInf*R-50;		% circulation
+xc = 1;						% x center coord
+yc = .5;					% y center coord
 xr = linspace(0,2,200);		% x points for grid
 yr = linspace(0,1,100);		% y points for grid
 [x,y] = meshgrid(xr,yr);	% meshgrid for calculating z values
-stagLine = lambda/2;		% stagnation line psi value
 
-colormap(winter);
+% elementary flow streamfunctions in cartesian (x,y)
+psiUniform = @(y0) vInf * (y-y0);
+psiSource = @(x0,y0) (lambda/(2*pi)) * atan2(y-y0, x-x0);
+psiDoublet = @(x0,y0) (-R^2 * vInf * (y-y0)) ./ ( (x-x0).^2 + (y-y0).^2);
+psiVortex = @(x0,y0) (circ/(2*pi)) * log(sqrt((x-x0).^2 + (y-y0).^2));
 
-% streamlines
-psi = vInf * (y - y0) + (lambda / (2 * pi)) * atan2(y - y0, x - x0);
-idxBody = abs(psi) < stagLine;
-psi(idxBody) = 0;
+% elementary flow velocities in cartesian (u = vel x, v = vel y)
+uUniform = vInf;
+vUniform = 0;
+uSource = @(x0,y0) (lambda/(2*pi)) * ( (x-x0) ./ ( (x-x0).^2 + (y-y0).^2 ) );
+vSource = @(x0,y0) (lambda/(2*pi)) * ( (y-y0) ./ ( (x-x0).^2 + (y-y0).^2 ) );
+uDoublet = @(x0,y0) (R^2 * vInf * ((y-y0).^2 - (x-x0).^2)) ./ ( (x-x0).^2 + (y-y0).^2 ).^2;
+vDoublet = @(x0,y0) (-2 * R^2 * vInf * (x-x0) .* (y-y0)) ./ ( (x-x0).^2 + (y-y0).^2 ).^2;
+uVortex = @(x0,y0) (circ * (y-y0)) ./ ( 2*pi * ( (x-x0).^2 + (y-y0).^2 ) );
+vVortex = @(x0,y0) (-circ * (x-x0)) ./ ( 2*pi * ( (x-x0).^2 + (y-y0).^2 ) );
 
-% velocity
-u = (lambda / (2 * pi)) * ((x - x0) ./ ((x - x0).^2 + (y - y0).^2)) + vInf;
-v = (lambda / (2 * pi)) * ((y - y0) ./ ((x - x0).^2 + (y - y0).^2));
-velMag = sqrt(u.^2 + v.^2);
-velMag(idxBody) = -1;
-velBins = [-1 0 .25*vInf .5*vInf .75*vInf vInf 1.25*vInf 1.5*vInf];
-%velBins = linspace(0,1.5*vInf,7);
+% flow combination streamlines
+psiHalfOval = psiUniform(yc) + psiSource(xc,yc);
+psiFullOval = psiUniform(yc) + psiSource(xc-b,yc) - psiSource(xc+b,yc);
+psiNonliftCylinder = psiUniform(yc) + psiDoublet(xc,yc);
+psiLiftCylinder = psiUniform(yc) + psiDoublet(xc,yc) + psiVortex(xc,yc);
+psiRandom1 = psiUniform(yc) + psiSource(xc-b,yc+.25*b) - psiSource(xc+2*b,yc+.25*b) + psiVortex(xc,yc)...
+	+ psiSource(xc-.25*b,yc-.25*b) - psiSource(xc+.25*b,yc-.25*b);
+psiRandom2 = psiUniform(yc) + psiSource(xc-b,yc) - psiSource(xc+b,yc+b) - psiSource(xc+b,yc-b)...
+	+ psiVortex(xc,yc+.25*b) - psiVortex(xc,yc-.25*b);
 
-% plot
+% flow combintation velocities
+velHalfOval = sqrt( (uUniform + uSource(xc,yc)).^2 + (vUniform + vSource(xc,yc)).^2 );
+velFullOval = sqrt( (uUniform + uSource(xc-b,yc) - uSource(xc+b,yc)).^2 +...
+	(vUniform + vSource(xc-b,yc) - vSource(xc+b,yc)).^2 );
+velNonliftCylinder = sqrt( (uUniform + uDoublet(xc,yc)).^2 + (vUniform + vDoublet(xc,yc)).^2 );
+velLiftCylinder = sqrt( (uUniform + uDoublet(xc,yc) + uVortex(xc,yc) ).^2 +...
+	(vUniform + vDoublet(xc,yc) + vVortex(xc,yc) ).^2 );
+velRandom1 = sqrt( (uUniform + uSource(xc-b,yc+.25*b) - uSource(xc+2*b,yc+.25*b) + uVortex(xc,yc)...
+	+ uSource(xc-.25*b,yc-.25*b) - uSource(xc+.25*b,yc-.25*b)).^2 + ...
+	(vUniform + vSource(xc-b,yc+.25*b) - vSource(xc+2*b,yc+.25*b) + vVortex(xc,yc)...
+	+ vSource(xc-.25*b,yc-.25*b) - vSource(xc+.25*b,yc-.25*b)).^2);
+velRandom2 = sqrt( (uUniform + uSource(xc-b,yc) - uSource(xc+b,yc+b) - uSource(xc+b,yc-b)...
+	+ uVortex(xc,yc+.25*b) - uVortex(xc,yc-.25*b)).^2 + ...
+	(vUniform + vSource(xc-b,yc) - vSource(xc+b,yc+b) - vSource(xc+b,yc-b)...
+	+ vVortex(xc,yc+.25*b) - vVortex(xc,yc-.25*b)).^2);
+
+if false
+
+% plot half oval
 fig1 = figure(1);
-[C,h] = contourf(x,y,velMag,velBins);
-clabel(C,h);
+colormap(jet);
+velBins = linspace(0,2*vInf,50);
+velBinLabels = [0 26.5306 51.0204 75.5102 100];
+[C,h] = contourf(x,y,velHalfOval,velBins);
+set(h,'LineStyle',':','Color','k');
+clabel(C,h,velBinLabels);
 pbaspect([2 1 1]);
 hold on;
-contour(x,y,psi,25,'k->');
+contour(x,y,psiHalfOval,25,'k-');
+stagLineIdx = psiHalfOval(abs(psiHalfOval) >= (lambda/2 - 0.1) & abs(psiHalfOval) <= (lambda/2 + 0.1));
+contour(x,y,psiHalfOval,stagLineIdx,'k-','Color','b','LineWidth',2);
+
+% plot full oval
+fig2 = figure(2);
+colormap(jet);
+velBins = linspace(0,2*vInf,50);
+velBinLabels = [0 26.5306 51.0204 75.5102 100];
+[C,h] = contourf(x,y,velFullOval,velBins);
+set(h,'LineStyle',':','Color','k');
+clabel(C,h,velBinLabels);
+pbaspect([2 1 1]);
+hold on;
+contour(x,y,psiFullOval,25,'k-');
+stagLineIdx = psiFullOval(abs(psiFullOval) >= (0 - 0.1) & abs(psiFullOval) <= (0 + 0.1));
+contour(x,y,psiFullOval,stagLineIdx,'k-','Color','b','LineWidth',2);
+
+% plot nonlifting cylinder
+fig3 = figure(3);
+colormap(jet);
+velBins = linspace(0,3*vInf,25);
+velBinLabels = [0 25 50 75 100];
+[C,h] = contourf(x,y,velNonliftCylinder,velBins);
+set(h,'LineStyle',':','Color','k');
+clabel(C,h,velBinLabels);
+pbaspect([2 1 1]);
+hold on;
+contour(x,y,psiNonliftCylinder,125,'k-');
+%stagLineIdx = psiNonliftCylinder(abs(psiNonliftCylinder) >= (0 - 0.1) & abs(psiNonliftCylinder) <= (0 + 0.1));
+%contour(x,y,psiNonliftCylinder,stagLineIdx,'k-','LineWidth',2);
+viscircles([xc,yc],R,'Color','b','LineWidth',2);
+
+% plot lifting cylinder
+fig4 = figure(4);
+colormap(jet);
+velBins = linspace(0,3*vInf,25);
+velBinLabels = [0 25 50 75 100];
+[C,h] = contourf(x,y,velLiftCylinder,velBins);
+set(h,'LineStyle',':','Color','k');
+clabel(C,h,velBinLabels);
+pbaspect([2 1 1]);
+hold on;
+contour(x,y,psiLiftCylinder,200,'k-');
+viscircles([xc,yc],R,'Color','b','LineWidth',2);
+
+% plot random 1
+fig5 = figure(5);
+colormap(jet);
+velBins = linspace(0,4*vInf,50);
+[C,h] = contourf(x,y,velRandom1,velBins);
+set(h,'LineStyle',':','Color','k');
+pbaspect([2 1 1]);
+hold on;
+contour(x,y,psiRandom1,25,'k-');
+
+end
+
+% plot random 2
+fig6 = figure(6);
+colormap(jet);
+velBins = linspace(0,4*vInf,50);
+[C,h] = contourf(x,y,velRandom2,velBins);
+set(h,'LineStyle','none');
+c = colorbar;
+c.Label.String = 'velocity (m/s)';
+title('Random combination (2), v_{\infty} = 50, \Lambda = 50');
+pbaspect([2 1 1]);
+hold on;
+contour(x,y,psiRandom2,50,'k-');
 
 % save plots to jpg
 %saveas(fig1,'halfOval.jpg');
-
-%=====================================================================
-% Rankine Full Oval
-%=====================================================================
-
-% variables
-vInf = 50;					% freestream velocity (m/s)
-lambda = 50;				% source / sink strenth
-b = .25;					% distance between source/sink and center
-x01 = 1.25;					% x center coord sink
-y01 = .5;					% y center coord sink
-x02 = .75;					% x center coord source
-y02 = .5;					% y center coord source
-xr = linspace(0,2,200);		% x points for grid
-yr = linspace(0,1,100);		% y points for grid
-[x,y] = meshgrid(xr,yr);	% meshgrid for calculating z values
-stagLine = 0;				% stagnation line psi value
-
-colormap(winter);
-
-% streamlines
-psi = vInf * (y - y0) + (lambda / (2 * pi)) * ( atan2(y - y02, x - x02) - atan2(y - y01, x - x01));
-psiTop = psi(1:50,:);
-psiBottom = psi(51:end,:);
-idxBodyTop = psiTop > (stagLine+0.0001);
-idxBodyBottom = psiBottom < (stagLine+0.0001);
-psiTop(idxBodyTop) = 0;
-psiBottom(idxBodyBottom) = 0;
-psi2 = [psiTop;psiBottom];
-%idxBody = psi(psi(100:end,50:end) < stagLine);
-%idxBody = psi(1:99,1:49) > stagLine;
-%psi(idxBody) = 0;
-
-% velocity
-u = ((lambda / (2 * pi)) * ((x - x02) ./ ((x - x02).^2 + (y - y02).^2))) -...
-((lambda / (2 * pi)) * ((x - x01) ./ ((x - x01).^2 + (y - y01).^2))) + vInf;
-v =  (lambda / (2 * pi)) * ((y - y02) ./ ((x - x02).^2 + (y - y02).^2)) -...
-(lambda / (2 * pi)) * ((y - y01) ./ ((x - x01).^2 + (y - y01).^2));
-velMag = sqrt(u.^2 + v.^2);
-%velMag(idxBody) = -1;
-velBins = [-1 0 .25*vInf .5*vInf .75*vInf vInf 1.25*vInf 1.5*vInf];
-velBins = linspace(0,1.5*vInf,7);
-
-% plot
-fig2 = figure(2);
-[C,h] = contourf(x,y,velMag,velBins);
-clabel(C,h);
-pbaspect([2 1 1]);
-hold on;
-contour(x,y,psi,25,'k->');
-
-%=====================================================================
-% Nonlifting Cylinder
-%=====================================================================
-
-% variables
-vInf = 50;					% freestream velocity (m/s)
-R = .25;					% radius of stagnation streamline
-x0 = 1;						% x center coord sink
-y0 = .5;					% y center coord sink
-xr = linspace(0,2,200);		% x points for grid
-yr = linspace(0,1,100);		% y points for grid
-[x,y] = meshgrid(xr,yr);	% meshgrid for calculating z values
-stagLine = 0;				% stagnation line psi value
-eps = 1e-6;					% small value to avoid a divide by zero
-
-colormap(winter);
-
-% streamlines
-psi = vInf * (y - y0) .* (1 - (R.^2 ./ ( (x-x0).^2 + (y-y0).^2 + eps^2 )));
-psiTop = psi(1:50,:);
-psiBottom = psi(51:end,:);
-idxBodyTop = psiTop > stagLine;
-idxBodyBottom = psiBottom < stagLine;
-psiTop(idxBodyTop) = -1;
-psiBottom(idxBodyBottom) = -1;
-psi2 = [psiTop;psiBottom];
-psiZero = psi(psi == 0);
-%idxBody = psi < stagLine;
-%psi(idxBody) = 0;
-%idxBodyTop = psiTop > (stagLine+0.0001);
-%psiTop(idxBodyTop) = 0;
-%psi2 = [psiTop;psiBottom];
-%idxBody = psi(psi(100:end,50:end) < stagLine);
-%idxBody = psi(1:99,1:49) > stagLine;
-%psi(idxBody) = 0;
-
-% velocity
-u = vInf * 2;
-v = (vInf * (-2) * (x - x0) .* (y - y0) * R.^2) ./ (( (x-x0).^2 + (y-y0).^2 + eps^2 ).^2);
-velMag = sqrt(u.^2 + v.^2);
-%velMag(idxBody) = -1;
-velBins = [-1 0 .25*vInf .5*vInf .75*vInf vInf 1.25*vInf 1.5*vInf];
-velBins = linspace(0,1.5*vInf,7);
-
-% plot
-fig3 = figure(3);
-[C,h] = contourf(x,y,velMag,velBins);
-clabel(C,h);
-pbaspect([2 1 1]);
-hold on;
-[C,h] = contour(x,y,psi,50,'k->');
-clabel(C,h);
 
