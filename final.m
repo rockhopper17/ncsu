1a43e6a8475960e71fea7652e84c8f06b0469381
Andrew Navratil <drew.navratil@gmail.com>
2018-11-21 06:39:17 -0500

    b4 movie concat

--- final.m
+++ final.m
@@ -18,233 +18,251 @@ global n m G
 % universal gravitational constant [km^3/kg/s^2]
 G = 6.67259e-20;      
 
-% step size values
+% default step size values, sometimes modified in the ic scenariois
 deltaT = 1*3600; % step size (hr * s/hr) [s]
-N = 60*(24*3600)/deltaT;  % num steps days * (hrs/day * s/hr)
-numFrameSkip = 10;		% only capture every numFrameSkip'th frame, animation code takes awhile
+N = 2*365*(24*3600)/deltaT;  % num steps days * (hrs/day * s/hr)
 
+% set this to true for writing out a movie file
+makemovie = false;
+numFrameSkip = 4;  % only capture every numFrameSkip'th frame, animation code takes awhile
+
+% ****************************************************************************************
 % initial conditions 
+% ****************************************************************************************
 % ic = 1: sun/earth/moon
 % ic = 2: inner solar system (sun/mercury/venus/earth/mars)
 % ic = 3: 3 body - 2 stars orbiting each other + 1 planet orbiting way out
 % ic = 4: 3body figure 8s w real m,G values (blows up sometime after 60 days)
 % ic = 5: 3body periodic solns w m=1, G=1
 % ic = 6: multiple solar systems interacting
-ic = 6;
+movienames = {'SunEarthMoon','innersol','3body1planet','3bodyFig8Real',...
+    '3bodyFig8G1','multiplesols'};
 
+ic = 2;  % set which scenario to execute ***********
+
+% switch on ic (initial conditions) for different scenarios
 if ic == 1
 
-n = 3;  % number of bodies
-m = zeros(1,n);  % masses
-m(1) = 1.9885e30;  % mass of sun [kg]
-m(2) = 5.974e24;    % mass of earth [kg]
-m(3) = 73.48e21;    % mass of moon [kg]
+	n = 3;  % number of bodies
+	m = zeros(1,n);  % masses
+	m(1) = 1.9885e30;  % mass of sun [kg]
+	m(2) = 5.974e24;    % mass of earth [kg]
+	m(3) = 73.48e21;    % mass of moon [kg]
 
-x(1) = 0;  % sun x init
-x(2) = 0;  % sun y init
-x(3) = 0;  % earth - x init
-x(4) = 1.4960e8;  % earth - y init
-x(5) = 0;  % moon - x init
-x(6) = x(4) + 3.844e5;  % moon - y init using avg distance to earth [km]
+	x(1) = 0;  % sun x init
+	x(2) = 0;  % sun y init
+	x(3) = 0;  % earth - x init
+	x(4) = 1.4960e8;  % earth - y init
+	x(5) = 0;  % moon - x init
+	x(6) = x(4) + 3.844e5;  % moon - y init using avg distance to earth [km]
 
-x(7) = 0; % sun x vel init
-x(8) = 0; % sun y vel init
-x(9) = -29.78;  % earth x vel init - mean orbital velocity [km/s]
-x(10) = 0;  % earth y vel init
-x(11) = x(9) - 1.022; % moon x vel init - mean orbital velocity of moon [km/s]
-x(12) = 0; % moon y vel init
+	x(7) = 0; % sun x vel init
+	x(8) = 0; % sun y vel init
+	x(9) = -29.78;  % earth x vel init - mean orbital velocity [km/s]
+	x(10) = 0;  % earth y vel init
+	x(11) = x(9) - 1.022; % moon x vel init - mean orbital velocity of moon [km/s]
+	x(12) = 0; % moon y vel init
 
 elseif ic == 2
 
-n = 5;  % number of bodies
-m = zeros(1,n);  % masses in [kg]
-m(1) = 1.9885e30;  % sun
-m(2) = 3.302e23;  % mercury
-m(3) = 4.869e24;  % venus
-m(4) = 5.974e24;  % earth 
-m(5) = 6.419e23;  % mars
+	n = 5;  % number of bodies
+	m = zeros(1,n);  % masses in [kg]
+	m(1) = 1.9885e30;  % sun
+	m(2) = 3.302e23;  % mercury
+	m(3) = 4.869e24;  % venus
+	m(4) = 5.974e24;  % earth 
+	m(5) = 6.419e23;  % mars
 
-%http://hyperphysics.phy-astr.gsu.edu/hbase/Solar/soldata2.html
-% positions in [km]
-x(1) = 0;  % sun x init
-x(2) = 0;  % sun y init
-x(3) = 0;  % mercury x init
-x(4) = 5.79e7; % mercury y init
-x(5) = 0;  % venus x init
-x(6) = 1.082e8; % venus y init
-x(7) = 0;  % earth x init
-x(8) = 1.4960e8;  % earth y init
-x(9) = 0;  % mars x init
-x(10) = 2.279e8;  % mars y init
+	%http://hyperphysics.phy-astr.gsu.edu/hbase/Solar/soldata2.html
+	% positions in [km]
+	x(1) = 0;  % sun x init
+	x(2) = 0;  % sun y init
+	x(3) = 0;  % mercury x init
+	x(4) = 5.79e7; % mercury y init
+	x(5) = 0;  % venus x init
+	x(6) = 1.082e8; % venus y init
+	x(7) = 0;  % earth x init
+	x(8) = 1.4960e8;  % earth y init
+	x(9) = 0;  % mars x init
+	x(10) = 2.279e8;  % mars y init
 
-% velocities in [km/s]
-x(11) = 0; % sun x vel init
-x(12) = 0; % sun y vel init
-x(13) = -47.4;  % mercury x vel init (mean)
-x(14) = 0;  % mercury y vel init
-x(15) = -35.0;  % venus x vel init (mean)
-x(16) = 0;  % venus y vel init
-x(17) = -29.8;  % earth x vel init (mean)
-x(18) = 0;  % earth y vel init
-x(19) = -24.1;  % mars x vel init
-x(20) = 0;  % mars y vel init
+	% velocities in [km/s]
+	x(11) = 0; % sun x vel init
+	x(12) = 0; % sun y vel init
+	x(13) = -47.4;  % mercury x vel init (mean)
+	x(14) = 0;  % mercury y vel init
+	x(15) = -35.0;  % venus x vel init (mean)
+	x(16) = 0;  % venus y vel init
+	x(17) = -29.8;  % earth x vel init (mean)
+	x(18) = 0;  % earth y vel init
+	x(19) = -24.1;  % mars x vel init
+	x(20) = 0;  % mars y vel init
 
 elseif ic == 3
 
-n = 3;  % number of bodies
-m = zeros(1,n);  % masses
-m(1) = 1.9885e30;  % star 1
-m(2) = 1.9885e30;  % star 2
-m(3) = 5.974e24;  % planet 1
+	deltaT = 1*3600; % step size (hr * s/hr) [s]
+	N = 2*(24*3600)/deltaT;  % num steps days * (hrs/day * s/hr)
 
-x(1) = -2e7;  % star 1 x init
-x(2) = 0;  % star 1 y init
-x(3) = 2e7;  % star 2 x init
-x(4) = 0;  % star 2 y init
-x(5) = 0;  % planet 1 x init
-x(6) = 5e8;  % planet 1 y init 
+	n = 3;  % number of bodies
+	
+	m = zeros(1,n);  % masses
+	m(1) = 1.9885e30;  % star 1
+	m(2) = 1.9885e30;  % star 2
+	m(3) = 5.974e24;  % planet 1
 
-x(7) = 0; % star 1 x vel init
-x(8) = -50; % star 1 y vel init
-x(9) = 0;  % star 2 x vel init 
-x(10) = 50;  % star 2 y vel init
-x(11) = -20; % planet 1 x vel init 
-x(12) = 0; % planet 1 y vel init
+	x(1) = -2e7;  % star 1 x init
+	x(2) = 0;  % star 1 y init
+	x(3) = 2e7;  % star 2 x init
+	x(4) = 0;  % star 2 y init
+	x(5) = 0;  % planet 1 x init
+	x(6) = 5e8;  % planet 1 y init 
+
+	x(7) = 0; % star 1 x vel init
+	x(8) = -50; % star 1 y vel init
+	x(9) = 0;  % star 2 x vel init 
+	x(10) = 50;  % star 2 y vel init
+	x(11) = -20; % planet 1 x vel init 
+	x(12) = 0; % planet 1 y vel init
 
 elseif ic == 4
 
-n = 3;  % number of bodies
-m = zeros(1,n);  % masses
-m(1) = 2e30;  % star 1
-m(2) = 2e30;  % star 2
-m(3) = 2e30;  % star 3
+	deltaT = 1*3600; % step size (hr * s/hr) [s]
+	N = 60*(24*3600)/deltaT;  % num steps days * (hrs/day * s/hr)
 
-x(1) = -1e7;  % star 1 x init
-x(2) = 0;  % star 1 y init
-x(3) = 1e7;  % star 2 x init
-x(4) = 0;  % star 2 y init
-x(5) = 0;  % star 3 x init
-x(6) = 0;  % star 3 y init 
+	n = 3;  % number of bodies
+	
+	m = zeros(1,n);  % masses
+	m(1) = 2e30;  % star 1
+	m(2) = 2e30;  % star 2
+	m(3) = 2e30;  % star 3
 
-x(7) = 125*0.34711; % star 1 x vel init
-x(8) = 125*0.53278; % star 1 y vel init
-x(9) = x(7);  % star 2 x vel init 
-x(10) = x(8);  % star 2 y vel init
-x(11) = -2*x(7); % star 3 x vel init 
-x(12) = -2*x(8); % star 3 y vel init
+	x(1) = -1e7;  % star 1 x init
+	x(2) = 0;  % star 1 y init
+	x(3) = 1e7;  % star 2 x init
+	x(4) = 0;  % star 2 y init
+	x(5) = 0;  % star 3 x init
+	x(6) = 0;  % star 3 y init 
 
+	x(7) = 125*0.34711; % star 1 x vel init
+	x(8) = 125*0.53278; % star 1 y vel init
+	x(9) = x(7);  % star 2 x vel init 
+	x(10) = x(8);  % star 2 y vel init
+	x(11) = -2*x(7); % star 3 x vel init 
+	x(12) = -2*x(8); % star 3 y vel init
 
 elseif ic == 5
 
-% this uses the data for 3-body orbits
-% where G = 1
-% http://three-body.ipb.ac.rs/
+	% this uses the data for 3-body orbits
+	% where G = 1
+	% http://three-body.ipb.ac.rs/
 
-% figure 8
-%p1 = 0.347111
-%p2 = 0.532728
-%T = 6.32449
+	% figure 8
+	p1 = 0.347111
+	p2 = 0.532728
+	T = 6.32449
 
-% butterfly (1)
-%p1 = 0.415251
-%p2 = 0.291346
-%T = 47.925856
+	% butterfly (1)
+	%p1 = 0.415251
+	%p2 = 0.291346
+	%T = 47.925856
 
-% butterfly (1)
-p1 = 0.184279
-p2 = 0.587188
-T = 63.534541
+	% butterfly (another one)
+	%p1 = 0.184279
+	%p2 = 0.587188
+	%T = 63.534541
 
-G = 1;
-deltaT = 0.1; % step size (hr * s/hr) [s]
-N = T*100/deltaT;  % num steps days * (hrs/day * s/hr)
-numFrameSkip = 10;		% only capture every numFrameSkip'th frame, animation code takes awhile
+	G = 1;
+	deltaT = 0.1; % step size (hr * s/hr) [s]
+	N = T*100/deltaT;  % num steps days * (hrs/day * s/hr)
+	numFrameSkip = 1; % only capture every numFrameSkip'th frame, animation code takes awhile
 
-n = 3;  % number of bodies
-m = zeros(1,n);  % masses
-m(1) = 1;  % star 1
-m(2) = 1;  % star 2
-m(3) = 1;  % star 3
+	n = 3;  % number of bodies
+	m = zeros(1,n);  % masses
+	m(1) = 1;  % star 1
+	m(2) = 1;  % star 2
+	m(3) = 1;  % star 3
 
-x(1) = -1;  % star 1 x init
-x(2) = 0;  % star 1 y init
-x(3) = 1;  % star 2 x init
-x(4) = 0;  % star 2 y init
-x(5) = 0;  % star 3 x init
-x(6) = 0;  % star 3 y init 
+	x(1) = -1;  % star 1 x init
+	x(2) = 0;  % star 1 y init
+	x(3) = 1;  % star 2 x init
+	x(4) = 0;  % star 2 y init
+	x(5) = 0;  % star 3 x init
+	x(6) = 0;  % star 3 y init 
 
-x(7) = p1; % star 1 x vel init
-x(8) = p2; % star 1 y vel init
-x(9) = p1;  % star 2 x vel init 
-x(10) = p2;  % star 2 y vel init
-x(11) = -2*p1; % star 3 x vel init 
-x(12) = -2*p2; % star 3 y vel init
+	x(7) = p1; % star 1 x vel init
+	x(8) = p2; % star 1 y vel init
+	x(9) = p1;  % star 2 x vel init 
+	x(10) = p2;  % star 2 y vel init
+	x(11) = -2*p1; % star 3 x vel init 
+	x(12) = -2*p2; % star 3 y vel init
 
 elseif ic == 6
 
-deltaT = 12*3600; % step size (hr * s/hr) [s]
-N = 365*35*(24*3600)/deltaT;  % num steps days * (hrs/day * s/hr)
-numFrameSkip = 100;		% only capture every numFrameSkip'th frame, animation code takes awhile
+	deltaT = 12*3600; % step size (hr * s/hr) [s]
+	N = 365*35*(24*3600)/deltaT;  % num steps days * (hrs/day * s/hr)
+	numFrameSkip = 100;		% only capture every numFrameSkip'th frame, animation code takes awhile
 
-n = 5*3;  % number of bodies
-m = zeros(1,n);  % masses in [kg]
-m(1) = 1.9885e30;  % sun
-m(2) = 3.302e23;  % mercury
-m(3) = 4.869e24;  % venus
-m(4) = 5.974e24;  % earth 
-m(5) = 6.419e23;  % mars
+	n = 5*3;  % number of bodies
+	m = zeros(1,n);  % masses in [kg]
+	m(1) = 1.9885e30;  % sun
+	m(2) = 3.302e23;  % mercury
+	m(3) = 4.869e24;  % venus
+	m(4) = 5.974e24;  % earth 
+	m(5) = 6.419e23;  % mars
 
-m(6:10) = m(1:5);
-m(11:15) = m(1:5);
-%m(16:20) = m(1:5);
-%m(21:25) = m(1:5);
+	m(6:10) = m(1:5);
+	m(11:15) = m(1:5);
+	%m(16:20) = m(1:5);
+	%m(21:25) = m(1:5);
 
-%http://hyperphysics.phy-astr.gsu.edu/hbase/Solar/soldata2.html
-% positions in [km]
-x(1) = 0;  % sun x init
-x(2) = 0;  % sun y init
-x(3) = 0;  % mercury x init
-x(4) = 5.79e7; % mercury y init
-x(5) = 0;  % venus x init
-x(6) = 1.082e8; % venus y init
-x(7) = 0;  % earth x init
-x(8) = 1.4960e8;  % earth y init
-x(9) = 0;  % mars x init
-x(10) = 2.279e8;  % mars y init
+	%http://hyperphysics.phy-astr.gsu.edu/hbase/Solar/soldata2.html
+	% positions in [km]
+	x(1) = 0;  % sun x init
+	x(2) = 0;  % sun y init
+	x(3) = 0;  % mercury x init
+	x(4) = 5.79e7; % mercury y init
+	x(5) = 0;  % venus x init
+	x(6) = 1.082e8; % venus y init
+	x(7) = 0;  % earth x init
+	x(8) = 1.4960e8;  % earth y init
+	x(9) = 0;  % mars x init
+	x(10) = 2.279e8;  % mars y init
 
-posoff = 5e9;
-x(11:20) = x(1:10) + posoff;
-x(21:2:29) = x(1:2:9) - posoff;
-x(22:2:30) = x(2:2:10) + posoff;
-%x(31:2:39) = x(1:2:9) - posoff;
-%x(32:2:40) = x(2:2:10) - posoff;
-%x(41:2:49) = x(1:2:9) + posoff;
-%x(42:2:50) = x(2:2:10) - posoff;
+	posoff = 5e9;
+	x(11:20) = x(1:10) + posoff;
+	x(21:2:29) = x(1:2:9) - posoff;
+	x(22:2:30) = x(2:2:10) + posoff;
+	%x(31:2:39) = x(1:2:9) - posoff;
+	%x(32:2:40) = x(2:2:10) - posoff;
+	%x(41:2:49) = x(1:2:9) + posoff;
+	%x(42:2:50) = x(2:2:10) - posoff;
 
-% velocities in [km/s]
-x(2*n+1) = 0; % sun x vel init
-x(2*n+2) = 0; % sun y vel init
-x(2*n+3) = -47.4;  % mercury x vel init (mean)
-x(2*n+4) = 0;  % mercury y vel init
-x(2*n+5) = -35.0;  % venus x vel init (mean)
-x(2*n+6) = 0;  % venus y vel init
-x(2*n+7) = -29.8;  % earth x vel init (mean)
-x(2*n+8) = 0;  % earth y vel init
-x(2*n+9) = -24.1;  % mars x vel init
-x(2*n+10) = 0;  % mars y vel init
+	% velocities in [km/s]
+	x(2*n+1) = 0; % sun x vel init
+	x(2*n+2) = 0; % sun y vel init
+	x(2*n+3) = -47.4;  % mercury x vel init (mean)
+	x(2*n+4) = 0;  % mercury y vel init
+	x(2*n+5) = -35.0;  % venus x vel init (mean)
+	x(2*n+6) = 0;  % venus y vel init
+	x(2*n+7) = -29.8;  % earth x vel init (mean)
+	x(2*n+8) = 0;  % earth y vel init
+	x(2*n+9) = -24.1;  % mars x vel init
+	x(2*n+10) = 0;  % mars y vel init
 
-veloff = 2;
-x(2*n+11:2*n+20) = x(2*n+1:2*n+10) - veloff;
-x(2*n+21:2:2*n+29) = x(2*n+1:2:2*n+9) + veloff;
-x(2*n+22:2:2*n+30) = x(2*n+2:2:2*n+10) - veloff;
-%x(2*n+31:2:2*n+39) = x(2*n+1:2:2*n+9) + veloff;
-%x(2*n+32:2:2*n+40) = x(2*n+2:2:2*n+10) + veloff;
-%x(2*n+41:2:2*n+49) = x(2*n+1:2:2*n+9) - veloff;
-%x(2*n+42:2:2*n+50) = x(2*n+2:2:2*n+10) + veloff;
+	veloff = 2;
+	x(2*n+11:2*n+20) = x(2*n+1:2*n+10) - veloff;
+	x(2*n+21:2:2*n+29) = x(2*n+1:2:2*n+9) + veloff;
+	x(2*n+22:2:2*n+30) = x(2*n+2:2:2*n+10) - veloff;
+	%x(2*n+31:2:2*n+39) = x(2*n+1:2:2*n+9) + veloff;
+	%x(2*n+32:2:2*n+40) = x(2*n+2:2:2*n+10) + veloff;
+	%x(2*n+41:2:2*n+49) = x(2*n+1:2:2*n+9) - veloff;
+	%x(2*n+42:2:2*n+50) = x(2*n+2:2:2*n+10) + veloff;
 
 end
 
+% ****************************************************************************************
 % main integration loop
+% ****************************************************************************************
 % time(i) = vector to hold time values
 % xgraph(1:n,i) i = time val, 1:n = x position values for each body at time i
 % ygraph(1:n,i) i = time val, 1:n = y position values for each body at time i
@@ -259,17 +277,14 @@ for i = 1:N
 	x = rk4('final_state',x,t,deltaT);
 end
 
-%if false
-
-% TODO: pick a month and find actual postion values for moon relative to earth
-% and plot that next to the numerical simulation
-
-% plot x position vs y position for moon
+% ****************************************************************************************
+% plot x position vs y position
+% ****************************************************************************************
 fig1 = figure(1);
-%colormap(jet); 
 hold on;
 grid on;
-title(sprintf('%d-body problem: x position vs y position',n));
+%title(sprintf('%d-body problem: x position vs y position',n));
+title(char(movienames(ic)));
 xlabel('x position (km)');
 ylabel('y position (km)');
 
@@ -277,20 +292,41 @@ ylabel('y position (km)');
 %xgraph(3,:) = xgraph(3,:) * 1.1;
 %ygraph(3,:) = ygraph(3,:) * 1.1;
 
-for i = 1:n
-	plot(xgraph(i,:),ygraph(i,:),'.-');
+if ic==2
+	% inner solar system
+	plot(xgraph(1,:),ygraph(1,:),'.','markersize',50,'color','yellow');
+	plot(xgraph(2,:),ygraph(2,:),'.-','color','magenta');	
+	plot(xgraph(3,:),ygraph(3,:),'.-','color','green');	
+	plot(xgraph(4,:),ygraph(4,:),'.-','color','blue');	
+	plot(xgraph(5,:),ygraph(5,:),'.-','color','red');	
+	
+	legend('sun','mercury','venus','earth','mars');
+else
+	for i = 1:n
+		plot(xgraph(i,:),ygraph(i,:),'.-');
+	end
 end
+
 set(gca,'color','black');
-%legend('sun','earth','moon');
+if ic == 4
+	axis([-1.5e7 1.5e7 -1.5e7 1.5e7]);
+elseif ic == 5
+	axis([-1.5 1.5 -1.5 1.5]);
+elseif ic == 6
+	axis([-1.5e10 1.5e10 -1e9 6e9]);
+else
+	axis([-2.5e8 2.5e8 -2.5e8 2.5e8]);
+end
 
-%end
-
-if false
+% ****************************************************************************************
+% animation code
+% ****************************************************************************************
+if makemovie == true
 
 % animation
 figure;
 hold on;
-writerObj = VideoWriter('nbody3sol.avi');
+writerObj = VideoWriter(char(movienames(ic)));
 %writerObj.FrameRate = 1/(deltaT*numFrameSkip);
 %writerObj.FrameRate = 30;
 writerObj.FrameRate = N/(30*numFrameSkip);  % N / desired length of movie in sec
@@ -308,9 +344,8 @@ end
 set(gca,'color','black');
 
 % first plot all the discs and just change their positions later on
-%   hard code some colors to fit some scenarios
 for j = 1:n
-	p(j) = plot(xgraph(j,1),ygraph(j,1),'.','markersize',25);
+	p(j) = plot(xgraph(j,1),ygraph(j,1),'.','markersize',50);
 	%if j==1
 		%%p(j) = plot(xgraph(j,1),ygraph(j,1),'.','markersize',50,'color',colors(1,:));
 		%p(j) = plot(xgraph(j,1),ygraph(j,1),'.','markersize',50,'color','red');
@@ -328,7 +363,7 @@ end
 % keep the same colors used above for the first few discs
 for i = numFrameSkip:numFrameSkip:N
 	for j = 1:n
-		plot(xgraph(j,1:numFrameSkip:i),ygraph(j,1:numFrameSkip:i),'-');
+		plot(xgraph(j,1:numFrameSkip:i),ygraph(j,1:numFrameSkip:i),'-w');
 		%if j==1
 			%plot(xgraph(j,1:numFrameSkip:i),ygraph(j,1:numFrameSkip:i),'-r');
 		%elseif j==2
@@ -341,7 +376,7 @@ for i = numFrameSkip:numFrameSkip:N
 
 		p(j).XData = xgraph(j,i);
 		p(j).YData = ygraph(j,i);
-		%drawnow;
+		drawnow;
 	end
 	
 	M = getframe;
@@ -352,4 +387,5 @@ end
 movie(M);
 close(writerObj);
 
-end
+end  % end animation code
+
