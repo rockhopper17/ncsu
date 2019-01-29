f25584929e6ea2e35e62cd1ba39f82ca260b38de
Andrew Navratil <drew.navratil@gmail.com>
2019-01-29 08:06:28 -0500

    implement icval=4 -> ex 2.3

--- ic.m
+++ ic.m
@@ -2,7 +2,8 @@
 % 1 = orig inner planets with rocket from mae361
 % 2 = ex 2.2 2D
 % 3 = ex 2.2 3D
-icval = 2
+% 4 = ex 2.3 3D
+icval = 3
 
 % initial condition swtich; ic value will be set in the main orbits.py file
 if icval == 1
@@ -11,6 +12,8 @@ if icval == 1
     N = 2*365*(24*3600)/deltaT;  % num steps days * (hrs/day * s/hr)
     runtime = 30;  % default num seconds to run each scenario
     %frate = N/(30*numFrameSkip)  # frame rate
+	frate = 60;  % frame rate for movie file, hard coded so all match when concatenating
+	numframes = frate * runtime;  % total number of frames to capture
 
     G = 6.67259e-20; % universal gravitational constant (km^3/kg/s^2) 
     n = 6;  % number of bodies total
@@ -26,20 +29,20 @@ if icval == 1
 
     %http://hyperphysics.phy-astr.gsu.edu/hbase/Solar/soldata2.html
     % initial positions (x y z) in (km)
-    r1 = [0,0,0]; % sun
-    r2 = [0,5.79e7,0]; % mercury
-    r3 = [0,1.082e8,0]; % venus
-    r4 = [0,1.496e8,0]; % earth
-    r5 = [0,-2.279e8,0]; % mars
-    r6 = [0,1.496e8,0]; % rocket (starts on earth)
+    r1 = [0,0]; % sun
+    r2 = [0,5.79e7]; % mercury
+    r3 = [0,1.082e8]; % venus
+    r4 = [0,1.496e8]; % earth
+    r5 = [0,-2.279e8]; % mars
+    r6 = [0,1.496e8]; % rocket (starts on earth)
 
     % initial velocities (vx, vy, vz) in (km/s)
-    v1 = [0,0,0];
-    v2 = [-47.4,0,0];
-    v3 = [-35.0,0,0];
-    v4 = [-29.8,0,0];
-    v5 = [24.1,0,0];
-    v6 = [-29.8,0,0];
+    v1 = [0,0];
+    v2 = [-47.4,0];
+    v3 = [-35.0,0];
+    v4 = [-29.8,0];
+    v5 = [24.1,0];
+    v6 = [-29.8,0];
 
     % state space array with initial conditions
     % the x vector holds x y z positions of each body and their velocities (derivatives)
@@ -89,6 +92,7 @@ elseif icval == 2
 
 elseif icval == 3
     % ex 2.2 in full 3D
+	% bookmatlabcode/twobody3d.m
 
     % initial timing conditions
     runtime = 480;  % total time to run sim (s)
@@ -111,6 +115,39 @@ elseif icval == 3
     % initial velocities (vx, vy, vz) in (km/s)
     v1 = [10,20,30];
     v2 = [0,40,0];
+
+    % state space array with initial conditions
+    % the x vector holds x y z positions of each body and their velocities (derivatives)
+    % x(0) = x2, x(1) = y1, x(2) = z1, x(3) = x2, x(4) = y2, x(5) = z2, ...
+    % x(3n) = x1 dot, x(3n+1) = y1 dot, x(3n+2) = z1 dot
+    x = [r1,r2,v1,v2];
+
+elseif icval == 4
+    % ex 2.3 in full 3D
+	% bookmatlabcode/orbit.m
+	% still doing this n-body algorithm, could look at doing 2 body with mu and r^3
+
+    % initial timing conditions
+    runtime = 4*3600;  % total time to run sim (hrs * sec/hr = sec)
+    deltaT = 60; % step size (s)
+    N = runtime / deltaT; % total number of iterations to run integration
+
+    G = 6.67259e-20; % universal gravitational constant (km^3/kg/s^2) 
+    n = 2;  % number of bodies total
+    ndim = 3; % num dimensions (3D: x,y,z)
+	
+	% masses [kg]
+    m = zeros(1,n); 
+    m(1) = 5.974e24;  % earth
+    m(2) = 1000;  % satellite
+
+    % initial positions (x y z) [km]
+    r1 = [0 0 0];
+    r2 = [8000 0 6000];
+
+    % initial velocities (vx, vy, vz) [km/s]
+    v1 = [0 0 0];
+    v2 = [0 7 0];
 
     % state space array with initial conditions
     % the x vector holds x y z positions of each body and their velocities (derivatives)
--- orbits.m
+++ orbits.m
@@ -10,41 +10,78 @@ ic
 makeplot = true;
 makemovie = false;
 
-% execute the integration loop and get plot data
+% ****************************************************************************************
+% main integration loop
+% ****************************************************************************************
 t = zeros(1,N);  % time values
 gd = zeros(N,n,ndim);  % graph data: time, body, position in x,y,z
-gdG = zeros(N,ndim);  % graph data for center of gravity in x,y,z
-summ = sum(m);  % sum of all masses for center of gravity calc
 d = struct('G',G,'m',m,'n',n,'ndim',ndim);  % data for passing to orbits_state
+
+if icval == 2 || icval == 3
+	summ = sum(m);  % sum of all masses for center of gravity calc
+	gdG = zeros(N,ndim);  % graph data for center of gravity in x,y,z
+elseif icval == 4
+	vd = zeros(N,n,ndim);  % velocity data
+end
 
 for i = 1:N
 	ti = i*deltaT;
     t(i) = ti;
   
     % pull out the positions from first half of x
-    % calculate center of gravity
 	for k = 1:ndim
         gd(i,:,k) = x(k:ndim:ndim*n);
-        %gdG(i,k) = sum(m .* gd(i,:,k)) / summ;
+
+		% for ex 2.3 we want the velocities too
+		if icval == 4
+        	vd(i,:,k) = x(n*ndim+k:ndim:end);
+		end
 	end
 
     % calculate center of gravity
-	for k = 1:ndim
-		sumj = 0;
-		for j = 1:n
-			sumj = sumj +  m(j) * gd(i,j,k);
-		end
+	if icval == 2 || icval == 3
+		for k = 1:ndim
+			sumj = 0;
+			for j = 1:n
+				sumj = sumj +  m(j) * gd(i,j,k);
+			end
 
-		gdG(i,k) = sumj / summ;
+			gdG(i,k) = sumj / summ;
+		end
 	end
 
     % using RK4 integrator	
     x = rk4(x, t, deltaT, d);
+
+	% inject impulsive delta V for inner sol rocket scenario
+	if icval == 1 & i == 8000
+		x(23) = x(23) + 2.75;  % add a 3 km/s burn in x dir
+		x(24) = x(24) - 3.5;  % add a 3 km/s burn in x dir
+	end
+
 end  % end time loop
 
-% show the plot of orbit paths
+% ****************************************************************************************
+% plots of orbits
+% ****************************************************************************************
 if makeplot
-	if icval == 2
+	if icval == 1
+		figure(1)
+		title('inner sol with rocket to mars')
+		hold on
+		grid on
+
+		for i = 1:n
+			plot(gd(:,i,1),gd(:,i,2),'.-','color',pltcolors(i,:))
+		end
+		
+		xlabel('x position (km)')
+		ylabel('y position (km)')
+		legend('sun','mercury','venus','earth','mars')
+		axis([-2.5e8 2.5e8 -2.5e8 2.5e8])
+		set(gca,'color','black')
+
+	elseif icval == 2
         figure(1)
         title('Figure 2.3: Motion relative to the inertial frame')
 		hold on
@@ -72,8 +109,8 @@ if makeplot
         plot3(gd(:,2,1), gd(:,2,2), gd(:,2,3), '-g')
 		plot3(gdG(:,1), gdG(:,2), gdG(:,3), '-b')
 
-        comet3(gd(:,1,1), gd(:,1,2), gd(:,1,3))
-        comet3(gd(:,2,1), gd(:,2,2), gd(:,2,3))
+        %comet3(gd(:,1,1), gd(:,1,2), gd(:,1,3))
+        %comet3(gd(:,2,1), gd(:,2,2), gd(:,2,3))
 
 		text(gd(1,1,1), gd(1,1,2), gd(1,1,3), '1', 'color', 'r')
 		text(gd(1,2,1), gd(1,2,2), gd(1,2,3), '2', 'color', 'g')
@@ -102,9 +139,141 @@ if makeplot
 		text(gd(1,2,1) - gdG(1,1), gd(1,2,2) - gdG(1,2), gd(1,2,3) - gdG(1,3), '2', 'color', 'g')
 
 		common_axis_settings
+	elseif icval == 4
+		% get radius values for satellite's orbit
+		for i = 1:N
+			r(i) = norm([gd(i,2,1) gd(i,2,2) gd(i,2,3)]);
+		end
+
+		% set earth values used below
+		R = 6378; % radius earth [km]
+		hours = 3600;
+
+		[rmax imax] = max(r);
+		[rmin imin] = min(r);
+
+		v_at_rmax   = norm([vd(imax,2,1) vd(imax,2,2) vd(imax,2,3)]);
+		v_at_rmin   = norm([vd(imin,2,1) vd(imin,2,2) vd(imin,2,3)]);
+
+		%...Output to the command window:
+		fprintf('\n\n--------------------------------------------------------\n')
+		fprintf('\n Earth Orbit\n')
+		fprintf(' %s\n', datestr(now))
+		fprintf('\n The initial position is [%g, %g, %g] (km).',...
+															 r2(1), r2(2), r2(3))
+		fprintf('\n   Magnitude = %g km\n', norm(r2))
+		fprintf('\n The initial velocity is [%g, %g, %g] (km/s).',...
+															 v2(1), v2(2), v2(3))
+		fprintf('\n   Magnitude = %g km/s\n', norm(v2))
+		fprintf('\n Initial time = %g h.\n Final time   = %g h.\n',0,runtime/hours) 
+		fprintf('\n The minimum altitude is %g km at time = %g h.',...
+					rmin-R, t(imin)/hours)
+		fprintf('\n The speed at that point is %g km/s.\n', v_at_rmin)
+		fprintf('\n The maximum altitude is %g km at time = %g h.',...
+					rmax-R, t(imax)/hours)
+		fprintf('\n The speed at that point is %g km/s\n', v_at_rmax)
+		fprintf('\n--------------------------------------------------------\n\n')
+
+		%...Plot the results:
+		%   Draw the planet
+		[xx, yy, zz] = sphere(100);
+		surf(R*xx, R*yy, R*zz)
+		colormap(light_gray)
+		caxis([-R/100 R/100])
+		shading interp
+
+		%   Draw and label the X, Y and Z axes
+		line([0 2*R],   [0 0],   [0 0]); text(2*R,   0,   0, 'X')
+		line(  [0 0], [0 2*R],   [0 0]); text(  0, 2*R,   0, 'Y')
+		line(  [0 0],   [0 0], [0 2*R]); text(  0,   0, 2*R, 'Z')
+
+		%   Plot the orbit, draw a radial to the starting point
+		%   and label the starting point (o) and the final point (f)
+		hold on
+		plot3(  gd(:,2,1),    gd(:,2,2),    gd(:,2,3),'k')
+		line([0 r2(1)], [0 r2(2)], [0 r2(3)])
+		text(   gd(1,2,1),    gd(1,2,2),    gd(1,2,3), 'o')
+		text( gd(end,2,1),  gd(end,2,2),  gd(end,2,3), 'f')
+
+		%   Select a view direction (a vector directed outward from the origin) 
+		view([1,1,.4])
+
+		%   Specify some properties of the graph
+		grid on
+		axis equal
+		xlabel('km')
+		ylabel('km')
+		zlabel('km')
+
 
 	end  % end icval
 end  % end makeplot
+
+
+% ****************************************************************************************
+% animation / movie %
+% ****************************************************************************************
+if makemovie
+	if icval == 1
+		% open movie file with frame rate calculated above and name from list
+		writerObj = VideoWriter('innersol.avi');
+
+		%frate = N/(runtime*numFrameSkip);  % frame rate
+		writerObj.FrameRate = frate;
+		open(writerObj);
+
+		% setup plot
+		figure
+		hold on
+
+		axis([-2.5e8 2.5e8 -2.5e8 2.5e8])
+
+		ph = zeros(n,1);  % array of plot handles so we can change positions
+		for j = 1:n
+			if j == 1
+				% sun
+				ph(j) = plot(gd(1,j,1),gd(1,j,2),'.','markersize',75,'color',pltcolors(j,:));
+			elseif j == 6
+				% rocket
+				ph(j) = plot(gd(1,j,1),gd(1,j,2),'<','markersize',5,'color',pltcolors(j,:));
+			else
+				% planets
+				ph(j) = plot(gd(1,j,1),gd(1,j,2),'.','markersize',40,'color',pltcolors(j,:));
+			end
+		end
+
+		% create legend one time, otherwise it will keep updating on each call to plot
+		[~, objh] = legend(ph(1:5),'sun','mercury','venus','earth','mars','AutoUpdate','off');
+		objhl = findobj(objh, 'type', 'line'); %// objects of legend of type line
+		set(objhl, 'Markersize', 25); %// set marker size as desired
+
+		set(gca,'color','black');
+
+		% start at 1*numFrameSkip, first point already plotted
+		% now plot all the orbit lines following the discs so the orbits are created as the disc moves
+		% keep the same colors used above for the first few discs
+		numFrameSkip = ceil(N/numframes);  % need to make sure we have correct num frames for frame rate
+		for i = numFrameSkip:numFrameSkip:N
+			for j = 1:n
+				plot(gd(1:numFrameSkip:i,j,1),gd(1:numFrameSkip:i,j,2),'-w')
+
+				set(ph(j),'XData',gd(i,j,1));  % update positions
+				set(ph(j),'YData',gd(i,j,2));
+				
+				drawnow
+			end
+
+			writeVideo(writerObj,getframe);
+		end  % end i (time loop)
+	
+		close(writerObj);
+
+	end  % end icval == 1
+end  % end makemovie
+
+% ****************************************************************************************
+%  functions %
+% ****************************************************************************************
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function common_axis_settings
@@ -123,6 +292,23 @@ ylabel('Y (km)')
 zlabel('Z (km)')
 end %common_axis_settings
 
+% ~~~~~~~~~~~~~~~~~~~~~~~
+function map = light_gray
+% ~~~~~~~~~~~~~~~~~~~~~~~
+%{
+  This function creates a color map for displaying the planet as light
+  gray with a black equator.
+  
+  r - fraction of red
+  g - fraction of green
+  b - fraction of blue
 
-% animation / movie
+%}
+% -----------------------
+r = 0.8; g = r; b = r;
+map = [r g b
+       0 0 0
+       r g b];
+end %light_gray
+
 
