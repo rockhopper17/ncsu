8090bcb3c1d13fd7e8f3c2224c0b693634bdccc3
Andrew Navratil <drew.navratil@gmail.com>
2019-01-31 06:18:39 -0500

    viewtypes 1 - 6

--- ExportData.m
+++ ExportData.m
@@ -3,13 +3,33 @@
 % clear all vars and plots
 close all; clear all; clc;
 
-fname = 'Scan_time.svd'
+% files to process, assumes time synced, will append all data points together
+svdmatname = 'svddata2.mat';  % data file name for saving tojj
+fnames = ["Scan_time.svd","Scan_time_plate.svd"];
+numf = numel(fnames);
 
-xyz = GetXYZCoordinates(fname,0);
+% init data structures to hold position and velocity values
+xyz = double.empty(0);
+amp_x = double.empty(0);
+amp_y = double.empty(0);
+amp_z = double.empty(0);
 
-[t, amp_x, usd_x] = GetPointData(fname, 'Time', 'Vib X', 'Velocity', 'Samples', 0, 0);
-[t, amp_y, usd_y] = GetPointData(fname, 'Time', 'Vib Y', 'Velocity', 'Samples', 0, 0);
-[t, amp_z, usd_z] = GetPointData(fname, 'Time', 'Vib Z', 'Velocity', 'Samples', 0, 0);
+% loop files and process
+for fidx = 1:numf
+    fname = fnames(fidx)
 
-% save all workspace variables
-save('svddata.mat');
+    xyz_t = GetXYZCoordinates(fname,0);
+
+    [t, amp_x_t, usd_x] = GetPointData(fname, 'Time', 'Vib X', 'Velocity', 'Samples', 0, 0);
+    [t, amp_y_t, usd_y] = GetPointData(fname, 'Time', 'Vib Y', 'Velocity', 'Samples', 0, 0);
+    [t, amp_z_t, usd_z] = GetPointData(fname, 'Time', 'Vib Z', 'Velocity', 'Samples', 0, 0);
+    
+    xyz = [xyz; xyz_t];
+    amp_x = [amp_x; amp_x_t];
+    amp_y = [amp_y; amp_y_t];
+    amp_z = [amp_z; amp_z_t];
+end
+
+% save desired workspace variables
+clear xyz_t amp_x_t amp_y_t amp_z_t fidx fname
+save(svdmatname);
