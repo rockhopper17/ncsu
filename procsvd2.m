b01b3a8692c3d67b05c8bac2350496ca299b12fe
Andrew Navratil <drew.navratil@gmail.com>
2019-04-09 14:34:21 -0400

    before mid row update ptype 4 to loop all

--- procsvd2.m
+++ procsvd2.m
@@ -35,6 +35,8 @@ close all; clear all; clc;
 %load('svddata_rect2.mat'); % test rectangle with cwt data
 
 load('svddata_4_y_CWT.mat'); % alum horn a/b=4 y
+%load('svddata_2_x_CWT.mat'); % alum horn a/b=2 x
+%load('svddata_exp4_y_CWT.mat'); % alum horn a/b= exp 4 y
 
 %svdtype = 'horn';
 svdtype = 'aaaa';
@@ -47,9 +49,12 @@ numpts = length(xyz); % num location points
 
 % pull out positions and scale to mm
 %x = xyz(:,1) * 1e3;
+%x = abs(xyz(:,1)) * 1e3; % flip the _2_x
 %y = xyz(:,2) * 1e3;
 y = xyz(:,1) * 1e3; % swap for y oriented horns
 x = xyz(:,2) * 1e3;
+%xpts = unique(round(x)); % need to round _2_x since it's misaligned
+%ypts = unique(round(y));
 xpts = unique(x);
 ypts = unique(y);
 
@@ -132,106 +137,122 @@ end
 %*****************************************************************************%
 % 1 = column plots of magnitude for all points in column
 % 2 = heat map with each point showing avg of magnitudes over full time range
-% 3 = column avg/max/row and total avg/max *** creates all 5 plots
-% 4 = presentation plots: time-domain, freq-domain, and time-freq domains
-plottype = 3
+% 3 = column avg/max/row and total avg/max *** creates 4 main plots
+% 4 = column value for each pt in a row *** creates 5th main plot
+% 5 = presentation plots: time-domain, freq-domain, and time-freq domains
+plottype = 4
 %*****************************************************************************%
 
-if plottype == 4
+if plottype == 5
 
-	npos = 726;
+	%npos = 726;
+	%npos = 359; % 4_y
+	npos = 349; % 4_y
+	%npos = 320; % 2_x
+	%npos = 412; % 4exp_y
 	sigx = amp_x(npos,:);
 
 	plot(t*1e6, sigx*1e3);
 	grid on;
-	title('time-domain for x velocity at point 726 (raw data)');
+	title([svdmatname([9,11]) ' time-domain for x velocity at point ' num2str(npos) ' (raw data)']);
 	ylabel('x-velocity [mm/s]','FontSize',24);
 	xlabel('time [\mus]','FontSize',24);
-	%set(gca,'FontSize',24);
-	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	set(gca,'FontSize',24);
+	%set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	saveas(gca,[svdmatname(1:end-4) '_xvel_' num2str(npos) '.jpg']);
+	drawnow
 
 	figure;
 	set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
 	pwelch(sigx,[],[],[],Fs);
-	title('Welch Power Spectral Density Estimate: Point 726');
-	%set(gca,'FontSize',24);
-	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	title([svdmatname([9,11]) ' Welch Power Spectral Density Estimate: Point ' num2str(npos)]);
+	set(gca,'FontSize',24);
+	%set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	saveas(gca,[svdmatname(1:end-4) '_pwelch_' num2str(npos) '.jpg']);
+	drawnow
 
 	% STFT
 	figure;
 	set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
 	colormap('jet');
 	pspectrum(sigx,Fs,'spectrogram','FrequencyLimits',[0 1000e3]);
-	title('time-frequency domain point 726: pspectrum, default');
+	title([svdmatname([9,11]) ' time-frequency domain point ' num2str(npos) ': pspectrum, default']);
 	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
 	colorbar;
+	saveas(gca,[svdmatname(1:end-4) '_pspectrum_' num2str(npos) '.jpg']);
+	drawnow
 
-	figure;
-	set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
-	colormap('jet');
-	pspectrum(sigx,Fs,'spectrogram','FrequencyLimits',[0 1000e3],'FrequencyResolution',11e3);
-	title('time-frequency domain point 726: pspectrum, Frequency Resolution = 11e3');
-	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
-	colorbar;
+	%figure;
+	%set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
+	%colormap('jet');
+	%pspectrum(sigx,Fs,'spectrogram','FrequencyLimits',[0 1000e3],'FrequencyResolution',11e3);
+	%title('time-frequency domain point 726: pspectrum, Frequency Resolution = 11e3');
+	%set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	%colorbar;
 
-	figure;
-	set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
-	colormap('jet');
-	pspectrum(sigx,Fs,'spectrogram','FrequencyLimits',[0 1000e3],'TimeResolution',1e-6);
-	title('time-frequency domain point 726: Time Resolution = 1e-6');
-	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
-	colorbar;
+	%figure;
+	%set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
+	%colormap('jet');
+	%pspectrum(sigx,Fs,'spectrogram','FrequencyLimits',[0 1000e3],'TimeResolution',1e-6);
+	%title('time-frequency domain point 726: Time Resolution = 1e-6');
+	%set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	%colorbar;
 
 	% CWT
 	figure;
 	set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
 	colormap('jet');
 	cwt(sigx,Fs,'FrequencyLimits',[0 1000e3]);
-	title('time-frequency domain point 726: cwt, default Morlet');
+	title([svdmatname([9,11]) ' time-frequency domain point ' num2str(npos) ': cwt, default Morlet']);
 	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
 	colorbar;
+	saveas(gca,[svdmatname(1:end-4) '_cwt_morlet_' num2str(npos) '.jpg']);
+	drawnow
 
 	figure;
 	set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
 	colormap('jet');
 	cwt(sigx,'bump',Fs,'FrequencyLimits',[0 1000e3]);
-	title('time-frequency domain point 726: cwt, bump');
+	title([svdmatname([9,11]) ' time-frequency domain point ' num2str(npos) ': cwt, bump']);
 	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
 	colorbar;
+	saveas(gca,[svdmatname(1:end-4) '_cwt_bump_' num2str(npos) '.jpg']);
+	drawnow
 
-elseif plottype == 3
-	% get colormap for making all lines different colors
-	cmap = jet(numel(xpts));
+elseif plottype == 4
+	% row number to plot
+	rownum = 13; % _4_y
+	% hard code all points in row 10 for misaligned horn
+	%rownum = 11; % _2_x
+	%ptsidxrow11 = [1052 1025 998 971 944 917 890 863 836 809 782 755 728 701 674 647 620 593 ...
+		%566 539 512 485 458 432 409 390 373 358 345 332 319 306 293 280 267 254 241 228 215 ...
+		%202 189 176 163 150 137 124 111 98 85 72 59 46 33 20 7];
 
 	% get num cols
 	numcols = numel(xpts);
+	%numcols = numel(ptsidxrow11);
 
-	% row number to plot
-	rownum = 15;
+	% get colormap for making all lines different colors
+	cmap = jet(numcols);
 
-	% max/avg/row data
-	sigxtmax = zeros(numcols,numt);
-	sigxtavg = zeros(numcols,numt);
+	% row data
 	sigxtrow = zeros(numcols,numt);
-	sigtmax = zeros(1,numt);
-	sigtavg = zeros(1,numt);
 	
 	% hold color map for scatter, and highlight points
 	sigxtcol = zeros(1,numcols);
 	rowpts = zeros(1,numcols);
-	hpts = zeros(1,numt);
 
 	% loop columns
 	for xidx = 1:numcols
 	%for xidx = [1 29 50]
 	%for xidx = [29]
-		% get indexes of all points in current column
+		% get indexes of all points in current column and row
 		ptsidx = find(x == xpts(xidx));
+		%ptsidx = find(round(x) == xpts(xidx));  % 2_x
 		ptsidxrow = find(x == xpts(xidx) & y == ypts(rownum));
+		%ptsidxrow = ptsidxrow11(xidx);
 
-		% get max/avg/row for the column
-		sigxtmax(xidx,:) = max(sigxt(ptsidx,:));
-		sigxtavg(xidx,:) = mean(sigxt(ptsidx,:));
+		% get pt in row
 		sigxtrow(xidx,:) = sigxt(ptsidxrow,:);
 
 		% set colors for scatter plot
@@ -241,12 +262,111 @@ elseif plottype == 3
 		xidx
 	end
 
+	%ptsidxrow = ptsidxrow11;
+
+	figure(1);
+	set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
+	colormap('jet');
+
+	% loop all points in row
+	%for xidx = 1:numel(ptsidxrow)
+	for xidx = 1:numel(rowpts)
+	%for xidx = [1 29 50]
+	%for xidx = [29]
+		%figure;
+		subplot(3,1,[1:2]);
+
+		% plot
+		%plot(t*1e6, sigxt(ptsidxrow(xidx),:), 'Color', cmap(sigxtcol(ptsidxrow(xidx)),:),'LineWidth',2);
+		plot(t*1e6, sigxt(rowpts(xidx),:), 'Color', cmap(sigxtcol(rowpts(xidx)),:),'LineWidth',2);
+		%title([svdmatname([9,11]) ' 300 kHz magnitude, bump cwt, all columns, row ' num2str(rownum)]);
+		title([svdmatname([9,11]) ' 300 kHz magnitude, Morlet cwt, all columns, row ' num2str(rownum)]);
+
+		hold on;
+	end
+
+	% configure plot
+	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	curtick = get(gca, 'YTick');
+	set(gca, 'YTickLabel', cellstr(num2str(curtick(:))));
+	ylabel('magnitude','FontSize',24);
+	xlabel('time [\mus]','FontSize',24);
+	grid on;
+	hold off;
+
+	% scatter plot map
+	subplot(3,1,3);
+	hold on; grid on;
+	set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+	%image(xImg, yImg, imageData, 'CDataMapping', 'scaled');
+	image(xImg, yImg, imrotate(flipdim(imageData,1),90), 'CDataMapping', 'scaled'); % ab4y
+	scatter(x, y, [], sigxtcol, 'filled');
+	%plot(x(ptsidxrow), y(ptsidxrow), 'or', 'MarkerSize', 10, 'LineWidth',2);
+	plot(x(rowpts), y(rowpts), 'or', 'MarkerSize', 10, 'LineWidth',2);
+	ylabel('y position [mm]','FontSize',24);
+	xlabel('x position [mm]','FontSize',24);
+	drawnow
+
+	if makemovie == true
+		writeVideo(writerObj,getframe(gcf));
+	elseif makeppt == true
+		imgname = sprintf('col%d.jpg',xidx);
+		saveas(gcf,imgname);
+		img = Picture(imgname);
+		%img.Width = '13.33in';
+		img.Width = '10.5in';
+		img.Height = '7.5in';
+		%img.Width = '1400px';
+		%img.Height = '1000px';
+		slide = add(slides,'Blank');
+		add(slide,img);
+	else
+		% save jpg
+		saveas(gcf,[svdmatname(1:end-4) '_row' num2str(rownum) '.jpg']);
+	end
+
+
+elseif plottype == 3
+	% get colormap for making all lines different colors
+	cmap = jet(numel(xpts));
+
+	% get num cols
+	numcols = numel(xpts);
+
+	% max/avg data
+	sigxtmax = zeros(numcols,numt);
+	sigxtavg = zeros(numcols,numt);
+	sigtmax = zeros(1,numt);
+	sigtavg = zeros(1,numt);
+	
+	% hold color map for scatter, and highlight points
+	sigxtcol = zeros(1,numcols);
+	hpts = zeros(1,numt);
+
+	% loop columns
+	for xidx = 1:numcols
+	%for xidx = [1 29 50]
+	%for xidx = [29]
+		% get indexes of all points in current column
+		ptsidx = find(x == xpts(xidx));
+		%ptsidx = find(round(x) == xpts(xidx));
+
+		% get max/avg for the column
+		sigxtmax(xidx,:) = max(sigxt(ptsidx,:));
+		sigxtavg(xidx,:) = mean(sigxt(ptsidx,:));
+
+		% set colors for scatter plot
+		sigxtcol(ptsidx) = xidx;
+		
+		xidx
+	end
+
 	% get total max/avg
 	[sigtmax,hpts] = max(sigxt(:,:));
 	sigtavg = mean(sigxt(:,:));
 
-	% 1 = all col max, 2 = all col avg, 3 = col row, 4 = max, 5 = avg
-	for ptype = 1:5
+	% 1 = avg, 2 = max, 3 = all col avg, 4 = all col max
+	for ptype = 1:4
 		figure(ptype);
 		set(gcf,'position',[200 200 1400 1000],'InvertHardCopy','off');
 		colormap('jet');
@@ -260,84 +380,48 @@ elseif plottype == 3
 
 			% plot
 			if ptype == 1
-				plot(t*1e6, sigxtmax(xidx,:), 'Color', cmap(xidx,:),'LineWidth',2);
-			elseif ptype == 2
-				plot(t*1e6, sigxtavg(xidx,:), 'Color', cmap(xidx,:),'LineWidth',2);
-			elseif ptype == 3
-				plot(t*1e6, sigxtrow(xidx,:), 'Color', cmap(xidx,:),'LineWidth',2);
-			elseif ptype == 4
-				plot(t*1e6, sigtmax,'-k','LineWidth',2);
-			elseif ptype == 5
 				plot(t*1e6, sigtavg,'-k','LineWidth',2);
+				%title([svdmatname([9,11]) ' 300 kHz magnitude, bump cwt, all points, avg']);
+				title([svdmatname([9,11]) ' 300 kHz magnitude, Morlet cwt, all points, avg']);
+			elseif ptype == 2
+				plot(t*1e6, sigtmax,'-k','LineWidth',2);
+				%title([svdmatname([9,11]) ' 300 kHz magnitude, bump cwt, all points, max']);
+				title([svdmatname([9,11]) ' 300 kHz magnitude, Morlet cwt, all points, max']);
+			elseif ptype == 3
+				plot(t*1e6, sigxtavg(xidx,:), 'Color', cmap(xidx,:),'LineWidth',2);
+				%title([svdmatname([9,11]) ' 300 kHz magnitude, bump cwt, all columns, avg']);
+				title([svdmatname([9,11]) ' 300 kHz magnitude, Morlet cwt, all columns, avg']);
+			elseif ptype == 4
+				plot(t*1e6, sigxtmax(xidx,:), 'Color', cmap(xidx,:),'LineWidth',2);
+				%title([svdmatname([9,11]) ' 300 kHz magnitude, bump cwt, all columns, max']);
+				title([svdmatname([9,11]) ' 300 kHz magnitude, Morlet cwt, all columns, max']);
 			end
 
 			hold on;
 		end
 
 		% configure plot
-		set(gca,'Color', [0.7,0.7,0.7], 'FontSize',14);
-
-		if ptype == 1
-			title('300 kHz magnitude, bump cwt, all columns, max', 'FontSize',14);
-			if strcmp(svdtype,'horn')
-				axis([0 250 0 0.1]); % horn
-			elseif strcmp(svdtype,'rect')
-				axis([0 250 0 0.03]); % rect
-			end
-		elseif ptype == 2
-			title('300 kHz magnitude, bump cwt, all columns, avg', 'FontSize',14);
-			if strcmp(svdtype,'horn')
-				axis([0 250 0 0.0125]); % horn
-			elseif strcmp(svdtype,'rect')
-				axis([0 250 0 0.005]); % rect
-			end
-		elseif ptype == 3
-			title(['300 kHz magnitude, bump cwt, all columns, row ' num2str(rownum)], 'FontSize',14);
-			if strcmp(svdtype,'horn')
-				axis([0 250 0 0.03]); % horn
-			elseif strcmp(svdtype,'rect')
-				axis([0 250 0 0.005]); % rect
-			end
-		elseif ptype == 4
-			title('300 kHz magnitude, bump cwt, all points, max', 'FontSize',14);
-			if strcmp(svdtype,'horn')
-				axis([0 250 0 0.1]); % horn
-			elseif strcmp(svdtype,'rect')
-				axis([0 250 0 0.03]); % rect
-			end
-		elseif ptype == 5
-			title('300 kHz magnitude, bump cwt, all points, avg', 'FontSize',14);
-			if strcmp(svdtype,'horn')
-				axis([0 250 0 0.005]); % horn
-			elseif strcmp(svdtype,'rect')
-				axis([0 250 0 0.005]); % rect
-			end
-		end
-
-		ylabel('magnitude','FontSize',14);
-		xlabel('time [\mus]','FontSize',14);
+		set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
+		curtick = get(gca, 'YTick');
+		set(gca, 'YTickLabel', cellstr(num2str(curtick(:))));
+		ylabel('magnitude','FontSize',24);
+		xlabel('time [\mus]','FontSize',24);
 		grid on;
 		hold off;
 
 		% scatter plot map
 		subplot(3,1,3);
 		hold on; grid on;
-		set(gca,'Color', [0.7,0.7,0.7], 'FontSize',14);
+		set(gca,'Color', [0.7,0.7,0.7], 'FontSize',24);
 		%image(xImg, yImg, imageData, 'CDataMapping', 'scaled');
-		image(xImg, yImg, imrotate(flipdim(imageData,1),90), 'CDataMapping', 'scaled'); % ab4y
+		%image(xImg, yImg, imrotate(imageData,180), 'CDataMapping', 'scaled'); % ab2x
+		image(xImg, yImg, imrotate(flipdim(imageData,1),90), 'CDataMapping', 'scaled'); % ab4y, ab4expy
 		scatter(x, y, [], sigxtcol, 'filled');
-		if ptype == 3
-			plot(x(rowpts), y(rowpts), 'or', 'MarkerSize', 10, 'LineWidth',2);
-		elseif ptype == 4
+		if ptype == 2
 			plot(x(hpts), y(hpts), 'or', 'MarkerSize', 10, 'LineWidth',2);
 		end
-		ylabel('y position [mm]','FontSize',14);
-		xlabel('x position [mm]','FontSize',14);
-		if strcmp(svdtype,'horn')
-			axis([-1 40 0 20]); % horn
-		elseif strcmp(svdtype,'rect')
-			axis([-2 40 0 20]); % rect
-		end
+		ylabel('y position [mm]','FontSize',24);
+		xlabel('x position [mm]','FontSize',24);
 		drawnow
 
 		if makemovie == true
@@ -353,6 +437,9 @@ elseif plottype == 3
 			%img.Height = '1000px';
 			slide = add(slides,'Blank');
 			add(slide,img);
+		else
+			% save jpg
+			saveas(gcf,[svdmatname(1:end-4) '_' num2str(ptype) '.jpg']);
 		end
 	end
 
